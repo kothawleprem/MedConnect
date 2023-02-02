a83a3d067850e4f894d9df7f9c517069cf4da014
Ganesh-coder18 <gs8783007@gmail.com>
2023-02-02 12:48:00 +0530

    Added UI for Login

---
+++ web/src/Exp.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Exp() {
+  return (
+    <div >
+      <h1> Hi</h1>
+    </div>
+  );
+}
+
+export default Exp;
---
+++ web/src/Otp.js
@@ -0,0 +1,73 @@
+import OTPInput from "react-otp-input";
+import React, { useState } from "react";
+import "./Otp.css";
+import Lottie from 'lottie-react';
+import login from './lotties/otp.json';
+
+function Otp() {
+  const [OTP, setOTP] = useState("");
+  function handleChange(OTP) {
+    setOTP(OTP);
+  }
+  const style = {
+    height: 400,
+    width: 400
+  };
+  
+  return (
+    <div className="otp-form">
+        <section className="otp-reg1" >
+            <div className="shadow p-3 mb-5 bg-white rounded">
+              <div className="container mt-3">
+                  <div className="reg-content1">
+                    <div className="reg-form01">
+                      <h2 className="form-title1" align="left"><span>Verify</span> Mail</h2><br></br>
+                      <form class="reg-form2" name="loginform" id="reg-form1" align="left" action="/Login" method="post">
+                        <div class="otp">
+                            <OTPInput
+                                onChange={handleChange}
+                                value={OTP}
+                                inputStyle="inputStyle"
+                                numInputs={4}
+                                // separator={<span>-</span>}
+                            />
+                        </div>
+                        <br></br>
+                        <div class="resend">
+                            <p className="p3">Didn't receive the code?</p>
+                            <a href="" className="p2">Resend</a>
+                        </div>
+                        <br></br>
+                        <div class="butt2">         
+                          <button  type="submit" name="submit" value="Submit">Verify</button>               
+                        </div>
+                                    
+                        {/* <div class="login1">
+                          <span class="forget2">
+                            <a href=''>
+                              <small>Create an account?</small>
+                            </a>        
+                          </span>
+                        </div> */}
+                        
+                        <div class="img1" align="left">
+                        <br></br><br></br>
+                          <Lottie 
+	                          animationData={login}
+                            style = {style}
+                          />
+                        </div>
+                      </form>
+                      <br/><br/><br></br><br></br>
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </section>
+      </div>
+ 
+
+  );
+}
+
+export default Otp;
