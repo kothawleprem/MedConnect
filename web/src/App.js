78abcd9f146017a6162f24ff9710ab8ec3ceb360
kothawleprem <kothawleprem@gmail.com>
2023-02-09 22:08:38 +0530

    Resolved merge conflicts

--- web/src/App.js
+++ web/src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Login from './Login';
 import Otp from './Otp';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path='/' exact component={Login} />
-        <Route path='/Otp' component={Otp} />
+        <Route path='/'  element={<Login/>} />
+        <Route path='/otp'   element={<Otp/>} />
       </Routes>
     </Router>
   );
--- web/src/Otp.js
+++ web/src/Otp.js
@@ -1,14 +1,50 @@
-import OTPInput from "react-otp-input";
+// import OTPInput from "react-otp-input";
+import { MuiOtpInput } from 'mui-one-time-password-input'
+
 import React, { useState } from "react";
 import "./Otp.css";
 import Lottie from 'lottie-react';
 import login from './lotties/otp.json';
+import { useNavigate } from "react-router-dom"
+import axios from "axios";
+
 
 function Otp() {
   const [OTP, setOTP] = useState("");
+  const [incorrect, setIncorrect] = useState(false)
+  const navigate = useNavigate();
+
   function handleChange(OTP) {
     setOTP(OTP);
   }
+
+ const submitOtp =()=>{
+  axios.post('http://127.0.0.1:8000/api/doctors/verify_email/', {
+    email: OTP,
+   
+  })
+  .then(function (response) {
+    console.log(response);
+    if(response.status==401){
+      console.log("Incorrect OTP")
+      setIncorrect(true);
+    
+    } else if(response.status=200) {
+     console.log("correct")
+     navigate(`/dashboard`)
+
+
+    } else{
+      console.log("try again")
+    }
+   
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+ }
+
   const style = {
     height: 400,
     width: 400
@@ -21,25 +57,23 @@ function Otp() {
               <div className="container mt-3">
                   <div className="reg-content1">
                     <div className="reg-form01">
-                      <h2 className="form-title1" align="left"><span>Verify</span> Mail</h2><br></br>
+                      <h2 className="form-title1" align="left"><span>Verify </span> Mail</h2><br></br>
                       <form class="reg-form2" name="loginform" id="reg-form1" align="left" action="/Login" method="post">
                         <div class="otp">
-                            <OTPInput
-                                onChange={handleChange}
-                                value={OTP}
-                                inputStyle="inputStyle"
-                                numInputs={4}
-                                // separator={<span>-</span>}
-                            />
+                         
+                             <MuiOtpInput value={OTP} onChange={handleChange} />
                         </div>
                         <br></br>
                         <div class="resend">
+
+                       {incorrect ? <p> Incorrect Otp!!</p> : <p> </p> } 
+
                             <p className="p3">Didn't receive the code?</p>
                             <a href="" className="p2">Resend</a>
                         </div>
                         <br></br>
                         <div class="butt2">         
-                          <button  type="submit" name="submit" value="Submit">Verify</button>               
+                          <button  onClick={submitOtp} type="submit" name="submit" value="Submit">Verify</button>               
                         </div>
                                     
                         {/* <div class="login1">
