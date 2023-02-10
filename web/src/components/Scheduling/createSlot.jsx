aa1f322996bade9b797d2a932b5c1213a39beb74
kothawleprem <kothawleprem@gmail.com>
2023-02-11 01:09:44 +0530

    Fix: Current date Issue

--- web/src/components/Scheduling/createSlot.jsx
+++ web/src/components/Scheduling/createSlot.jsx
@@ -10,79 +10,79 @@ import Tabs from "react-bootstrap/Tabs";
  import "react-toastify/dist/ReactToastify.css";
 
 const CreateSlot = () => {
-    const [startTime, setStartTime] = useState("");
-    const [endTime, setEndTime] = useState("");
-    const [time, setTime] = React.useState("10:00");
-    const today = new Date().toISOString().substr(0, 10);
-    const tomorrow = new Date(Date.now() + 24 * 60 * 60 * 1000)
-      .toISOString()
-      .substr(0, 10);
-    // console.log(today, tomorrow)
-    // const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000);
-    // const tod = {today.toLocaleDateString()}
-    
-    const handleChangeStart = (e) => {
-        e.preventDefault();
-      setStartTime({
-        ...startTime,
-        [e.target.name]: e.target.value.trim(),
-      });
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [time, setTime] = React.useState("10:00");
+
+  var tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
+  var localISOTime = new Date(Date.now() - tzoffset).toISOString().slice(0, -1);
+  var tlocalISOTime = new Date(Date.now() - tzoffset + 24 * 60 * 60 * 1000)
+    .toISOString()
+    .slice(0, -1);
+  const today = localISOTime.substring(0, 10);
+  const tomorrow = tlocalISOTime.substring(0, 10);
+  console.log(today, tomorrow)
+
+  const handleChangeStart = (e) => {
+    e.preventDefault();
+    setStartTime({
+      ...startTime,
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+  const handleChangeEnd = (e) => {
+    e.preventDefault();
+    setEndTime({
+      ...startTime,
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+  const handleSubmit = (day) => {
+    console.log("submit");
+    console.log(startTime["start_time"], endTime["end_time"], day);
+    const req = {
+      doctor_id: 3,
+      date: day,
+      start_time: startTime["start_time"],
+      end_time: endTime["end_time"],
+      remarks: "NA",
     };
-    const handleChangeEnd = (e) => {
-        e.preventDefault();
-      setEndTime({
-        ...startTime,
-        [e.target.name]: e.target.value.trim(),
-      });
-    }; 
-     const handleSubmit = (day) => {
-      console.log("submit")
-       console.log(startTime['start_time'], endTime['end_time'], day)
-       const req = {
-         doctor_id: 3,
-         date: day,
-         start_time: startTime["start_time"],
-         end_time: endTime["end_time"],
-         remarks: "NA",
-       };
-       fetch(`http://127.0.0.1:8000/api/consultation/slot/`, {
-         method: "POST",
-         headers: {
-           Accept: "application/json",
-           "Content-Type": "application/json",
-         },
-         body: JSON.stringify(req),
-       }).then(async (response) => {
-         const result = await response.json();
-         console.log(result);
-         console.log(response.status, result['message'])
-         if(response.status === 201){
-           toast.success("Slot Created,please refresh", {
-             position: "top-right",
-             autoClose: 5000,
-             hideProgressBar: false,
-             closeOnClick: true,
-             pauseOnHover: true,
-             draggable: true,
-             progress: undefined,
-             theme: "light",
-           });
-         }
-         else{
-           toast.warn(result["message"], {
-             position: "top-right",
-             autoClose: 5000,
-             hideProgressBar: false,
-             closeOnClick: true,
-             pauseOnHover: true,
-             draggable: true,
-             progress: undefined,
-             theme: "light",
-           });
-         }
-        
-       });
-     };
+    fetch(`http://127.0.0.1:8000/api/consultation/slot/`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(req),
+    }).then(async (response) => {
+      const result = await response.json();
+      console.log(result);
+      console.log(response.status, result["message"]);
+      if (response.status === 201) {
+        toast.success("Slot Created,please refresh", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      } else {
+        toast.warn(result["message"], {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    });
+  };
   return (
     <>
       <h1>Select Date and Time</h1>
