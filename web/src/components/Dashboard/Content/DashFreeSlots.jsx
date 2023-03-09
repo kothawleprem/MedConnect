8828fe232390a886ea6018848b95e0648d0ca523
shubhamsaroj23 <shubhamsaroj197@gmail.com>
2023-03-09 23:03:35 +0530

    developed  dashboard ui

--- web/src/components/Dashboard/Content/DashFreeSlots.jsx
+++ web/src/components/Dashboard/Content/DashFreeSlots.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Row, Col, Card } from "react-bootstrap";
+import { Row, Col, Card, Button } from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
 
 
@@ -8,28 +8,77 @@ const DashFreeSlots = () => {
     <div >
         <Card style={{margin:'10px'}}>
       
-        <Card.Header>Free Slots for Today</Card.Header>
+        <Card.Header style={{fontWeight:'500'}}> Free Slots for Today </Card.Header>
 
         <Container>
-      <Row>
-        <Col>
+      
 
-         <div style={{margin:'10px'}} >
-          <div className='slot-button'> 
-         9:30
-          </div>
-
-         </div>
-         
+      <Row  style={{margin:'10px'}}>
+        <Col xl={4} lg={4} md={4} sm={12}>
+         <Button className='st-butt rounded-pill'>9:00 to 9:30 
+        <img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+        </Button>
          </Col>
-        <Col xs={5}>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
+         
+
+
+        <Col xl={4} lg={4} md={4} sm={12}>
+        <Button className='st-butt rounded-pill'>9:00 to 9:30 
+        <img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+        </Button>
+        </Col>
+
+        <Col xl={4} lg={4} md={4} sm={12}>
+        <Button className='st-butt rounded-pill'>9:00 to 9:30 
+        <img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+        </Button>
+        </Col>
+
       </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col xs={5}>2 of 3 (wider)</Col>
-        <Col>3 of 3</Col>
+      <Row style={{margin:'10px'}}>
+
+        <Col>
+        <Button className='st-butt rounded-pill'>9:00 to 9:30 
+        <img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+        </Button>
+        </Col>
+
+        <Col>
+        <Button className='st-butt rounded-pill'>9:00 to 9:30 
+        <img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+        </Button>
+        </Col>
+
+        <Col>
+        <Button className='st-butt rounded-pill'>9:00 to 9:30 
+        <img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+        </Button>
+        </Col>
+
       </Row>
+
+
+      <Row style={{margin:'10px'}}>
+
+<Col>
+<Button className='st-butt rounded-pill'>9:00 to 9:30 
+<img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+</Button>
+</Col>
+
+<Col>
+<Button className='st-butt rounded-pill'>9:00 to 9:30 
+<img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+</Button>
+</Col>
+
+<Col>
+<Button className='st-butt rounded-pill'>9:00 to 9:30 
+<img alt="" src="/Assets/add-selection.png"  className="st-butticon"/>
+</Button>
+</Col>
+
+</Row>
     </Container>
    
         </Card>
