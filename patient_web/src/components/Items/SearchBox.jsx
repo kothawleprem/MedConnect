1461877721b3cd301b9c56a07c5aa9ed1c96f6fb
kothawleprem <kothawleprem@gmail.com>
2023-04-10 10:19:39 +0530

    Completed Doctor Search Feature

---
+++ patient_web/src/components/Items/SearchBox.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Container, Form, Button } from "react-bootstrap";
+
+import { useState } from "react";
+
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const SearchBox = (props) => {
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+    const handleClick = () => {
+      // make API request with the query data
+      axios
+        .get(`http://127.0.0.1:8000/api/patients/search/?query=${query}`)
+        .then(function (response) {
+          const data = response.data;
+          props.onSearch(data);
+        //   console.log(data);
+        });
+    navigate(`/search?query=${query}`);
+    
+    };
+  return (
+    <>
+      <Container>
+        <br />
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Button variant="outline-success" onClick={handleClick} >
+            Search
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+}
+
+export default SearchBox
